09a262562165e1418bc225995a6cf9c4ee20c79b
TieDall <{ID}+{username}@users.noreply.github.com>
2021-04-05 19:27:06 +0200

    feat: gutschein

--- WebApi/Entities/Gutschein.cs
+++ WebApi/Entities/Gutschein.cs
@@ -11,5 +11,8 @@ namespace WebApi.Entities
         public decimal Betrag { get; set; }
         public DateTime? Ablaufdatum { get; set; }
         public string Bemerkung { get; set; }
+
+        public bool IsOneMonthLeft => this.Ablaufdatum?.AddMonths(-1) < DateTime.Now;
+        public bool IsHalfYearLeft => this.Ablaufdatum?.AddMonths(-6) < DateTime.Now;
     }
 }
