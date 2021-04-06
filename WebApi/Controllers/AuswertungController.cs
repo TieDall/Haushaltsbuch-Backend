f0f0d0fcebdc4c16791871ff2b1a8b2322be9aba
TieDall <{ID}+{username}@users.noreply.github.com>
2021-04-06 22:06:49 +0200

    feat: reporting

--- WebApi/Controllers/AuswertungController.cs
+++ WebApi/Controllers/AuswertungController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +23,6 @@ namespace WebApi.Controllers
         {
             return CalcVermoegen();
         }
-
-        [HttpGet("GetBilanz/{beginnYear}/{beginnMonth}/{endYear}/{endMonth}")]
-        public async Task<ActionResult<decimal>> GetBilanz(int beginnYear, int beginnMonth, int endYear, int endMonth) { return null; }
 
         [HttpGet("GetVermoegen/{beginnYear}/{beginnMonth}/{endYear}/{endMonth}")]
         public async Task<ActionResult<List<decimal>>> GetVermoegen(int beginnYear, int beginnMonth, int endYear, int endMonth) 
@@ -143,6 +141,108 @@ namespace WebApi.Controllers
                 startKapital
                 + sumBuchungEinnahme - sumBuchungAusgabe
                 + sumMonatlicheDauerauftraege + sumQuartalsweiseDauerauftraege + sumJaehrlicheDauerauftraege;
+
+            return result;
+        }
+
+        [HttpGet("GetBilanz")]
+        public async Task<ActionResult<decimal>> GetCurrentBilanz() 
+        {
+            return CalcBilanz();
+        }
+
+        [HttpGet("GetBilanz/{beginnYear}/{beginnMonth}/{endYear}/{endMonth}")]
+        public async Task<ActionResult<List<decimal>>> GetBilanz(int beginnYear, int beginnMonth, int endYear, int endMonth)
+        {
+            var result = new List<decimal>();
+
+            while (beginnYear != endYear || beginnMonth != endMonth)
+            {
+                result.Add(CalcBilanz(beginnYear, beginnMonth));
+
+                if (beginnMonth < 12)
+                {
+                    beginnMonth++;
+                }
+                else
+                {
+                    beginnYear++;
+                    beginnMonth = 1;
+                }
+            }
+            result.Add(CalcBilanz(beginnYear, beginnMonth));
+
+            return result;
+        }
+
+        private decimal CalcBilanz(int? year = null, int? month = null)
+        {
+            decimal result = 0;
+
+            // Set Current Month und Year
+            var monthToReturn = new DateTime();
+            if (year == null || month == null)
+            {
+                monthToReturn = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month));
+            }
+            else
+            {
+                monthToReturn = new DateTime((int)year, (int)month, DateTime.DaysInMonth((int)year, (int)month));
+            }
+
+            var monthStart = new DateTime(monthToReturn.Year, monthToReturn.Month, 1);
+            var monthEnd = new DateTime(monthToReturn.Year, monthToReturn.Month, DateTime.DaysInMonth(monthToReturn.Year, monthToReturn.Month));
+
+            // Ermittle Summe Buchungen
+            var buchungen = _context.Buchungen.Where(x => x.Buchungstag.Month == monthToReturn.Month && x.Buchungstag.Year == monthToReturn.Year);
+            var sumBuchungEinnahme = buchungen.Where(x => x.IsEinnahme).Sum(x => x.Betrag);
+            var sumBuchungAusgabe = buchungen.Where(x => !x.IsEinnahme).Sum(x => x.Betrag);
+
+            // Ermittle Summe Daueraufträge
+            var dauerauftraege = _context.Dauerauftraege
+                .Include(x => x.Kategorie)
+                .Where(x =>
+                    (
+                        x.Intervall == Enums.Intervall.monatlich &&
+                        DateTime.Compare(x.Beginn, monthEnd) <= 0 &&
+                        (x.Ende == null || DateTime.Compare((DateTime)x.Ende, monthStart) >= 0)
+                    )
+                    ||
+                    (
+                        x.Intervall == Enums.Intervall.quartalsweise
+                        && DateTime.Compare(x.Beginn, monthEnd) <= 0
+                        && (x.Ende == null || DateTime.Compare((DateTime)x.Ende, monthStart) >= 0)
+                        &&
+                        (
+                            ((x.Beginn.Month == 1 || x.Beginn.Month == 4 || x.Beginn.Month == 7 || x.Beginn.Month == 10)
+                            &&
+                            (month == 1 || month == 4 || month == 7 || month == 10))
+                            ||
+                            ((x.Beginn.Month == 2 || x.Beginn.Month == 5 || x.Beginn.Month == 8 || x.Beginn.Month == 11)
+                            &&
+                            (month == 2 || month == 5 || month == 8 || month == 11))
+                            ||
+                            ((x.Beginn.Month == 3 || x.Beginn.Month == 6 || x.Beginn.Month == 9 || x.Beginn.Month == 12)
+                            &&
+                            (month == 3 || month == 6 || month == 9 || month == 12))
+                        )
+                    )
+                    ||
+                    (
+                        x.Intervall == Enums.Intervall.jaehrlich
+                        && DateTime.Compare(x.Beginn, monthEnd) <= 0
+                        && (x.Ende == null || DateTime.Compare((DateTime)x.Ende, monthStart) >= 0)
+                        && x.Beginn.Month == month
+                    )
+                )
+                .ToList();
+
+            var sumDauerauftragEinnahme = dauerauftraege.Where(x => x.IsEinnahme).Sum(x => x.Betrag);
+            var sumDauerauftragAusgabe = dauerauftraege.Where(x => !x.IsEinnahme).Sum(x => x.Betrag);
+
+            result = 
+                sumBuchungEinnahme - sumBuchungAusgabe
+                + sumDauerauftragEinnahme - sumDauerauftragAusgabe;
 
             return result;
         }
