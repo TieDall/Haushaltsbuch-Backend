3378137e4219d1b133bd8645991c9468eeb6778b
TieDall <toischro@outlook.com>
2021-05-24 09:03:46 +0200

    add: Konfiguration Start Seed

---
+++ WebApi/Migrations/MySqlMigrations/20210524070247_KonfigurationStart.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations.MySqlMigrations
+{
+    public partial class KonfigurationStart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Konfigurationen(Parameter, Wert) VALUES ('Start', 0)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
