a6de128c0870632962e7703b3ff408d5b0716460
TieDall <toischro@outlook.com>
2021-12-20 12:00:33 +0100

    refactore(Buchung): included base crud controller

--- Controller/SimpleController/BuchungController.cs
+++ Controller/SimpleController/BuchungController.cs
@@ -2,50 +2,23 @@
 using DataServices.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Controller.SimpleController
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BuchungController : ControllerBase
+    public class BuchungController : CrudController<BusinessModels.Buchung, DataServices.Entities.Buchung>
     {
         private readonly IBuchungDataService _buchungDataService;
 
         public BuchungController(
-            IBuchungDataService buchungDataService) 
+            IBuchungDataService buchungDataService) : base(buchungDataService)
         {
             _buchungDataService = buchungDataService;
         }
 
-        [HttpPost]
-        public async Task<ActionResult<Buchung>> AddBuchung(Buchung buchung) 
-        {
-            var result = await _buchungDataService.Add(buchung);
-            return CreatedAtAction(nameof(GetBuchung), new { id = result.Id }, result);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Buchung>> GetBuchung(long id) 
-        {
-            return Ok(await _buchungDataService.Get(id));
-        }
-
-        [HttpPut("{id}")]
-        public async Task<ActionResult<Buchung>> UpdateBuchung(long id, Buchung buchung) 
-        {
-            return Ok(await _buchungDataService.Update(buchung));
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBuchung(long id) 
-        {
-            await _buchungDataService.Delete(id);
-            return Ok();
-        }
-
         [HttpGet("GetByMonth/{year}/{month}")]
-        public async Task<ActionResult<IEnumerable<Buchung>>> GetBuchungenByMonth(int year, int month)
+        public ActionResult<IEnumerable<Buchung>> GetBuchungenByMonth(int year, int month)
         {
             return Ok(_buchungDataService.GetByMonth(year, month));
         }
